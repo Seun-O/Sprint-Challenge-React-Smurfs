06153443b523a6650f7e106c6da25e0d85a3769e
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-04-07 21:33:10 -0400

    Axios get request functioning properly

--- village/src/App.js
+++ village/src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import React, { Component } from "react";
+import axios from "axios";
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
@@ -21,6 +21,12 @@ class App extends Component {
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
+  }
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3333/smurfs`)
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch();
   }
 }
 
